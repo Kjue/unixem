ef89aff8c97ce534d10932b9b8faa673131b01cc
lisongmin <lisongmin@126.com>
2013-07-10 22:43:22 +0800

    build unixem under mingw2.0.8 with gcc 4.8.1

---
+++ build.bat
@@ -0,0 +1,7 @@
+﻿
+rmdir /s /q aa
+mkdir aa
+cd aa
+cmake -DCMAKE_TOOLCHAIN_FILE=..\cmake\toolchain-mingw64.cmake -G "MinGW Makefiles" ..
+make -j3
+cd ..
---
+++ include/fsync.h
@@ -0,0 +1,15 @@
+
+#ifndef __FSYNC_H__
+#define __FSYNC_H__
+
+#ifdef __cplusplus
+extern "c" {
+#endif // __cplusplus
+
+int fsync (int fd);
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif //__FSYNC_H__
