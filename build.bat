0662043415667995c233f4554d39ac52ba4f4473
lisongmin <lsm@skybility.com>
2013-07-17 16:56:04 +0800

    output *.a to lib dir.

--- build.bat
+++ build.bat
@@ -2,6 +2,7 @@
 rmdir /s /q aa
 mkdir aa
 cd aa
-cmake -DCMAKE_TOOLCHAIN_FILE=..\cmake\toolchain-mingw64.cmake -G "MinGW Makefiles" ..
+cmake -DCMAKE_TOOLCHAIN_FILE=..\cmake\toolchain-mingw64.cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release ..
 make -j3
+make install
 cd ..
